90a3ad60e40df0a145bb363562c18b3a56a7c578
Chris Cooper <cjcooper252@gmail.com>
2016-11-18 16:29:03 +0000

    complete

--- lib/journey_log.rb
+++ lib/journey_log.rb
@@ -1,33 +1,35 @@
 require_relative 'journey.rb'
 require_relative 'station.rb'
 
+
 class JourneyLog
+
+  attr_reader :journeys, :journey_klass
+  attr_accessor :current_journey
 
   def initialize(journey_klass)
     @journey_klass = journey_klass
     @journeys = []
   end
 
-  def journeys
-    @journeys.dup
-  end
-
   def start(station)
-    @current_journey = @journey_klass.new(station)
-    @journeys << @current_journey
+    self.current_journey = journey_klass.new(station)
+    self.journeys << current_journey
   end
 
   def finish(station)
-      if @current_journey
-        @current_journey.end(station)
+      if current_journey
+        current_journey.end(station)
       else
-        @current_journey = @journey_klass.new
-        @journeys << @current_journey
-        @current_journey.end(station)
+        self.current_journey = journey_klass.new
+        self.journeys << current_journey
+        current_journey.end(station)
       end
 
-      @current_journey = nil
+      self.current_journey = nil
   end
+
+
 
   # def last_journey
   #   @journeys.last
